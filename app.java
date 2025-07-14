bcc12ab01f1a9615518074d5dd5a5188f9965aae
Arumbha <arumbhapaikaray123@gmail.com>
2025-07-14 19:49:16 +0530

    Update app.java

--- app.java
+++ app.java
@@ -6,16 +6,5 @@ public class app {
 
         // Print Hello World
         System.out.println("Hello, World!");
-
-        // Take two numbers from user
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
-
-        // Add the numbers
-        int sum = num1 + num2;
-        System.out.println("The sum is: " + sum);
     }
 }
